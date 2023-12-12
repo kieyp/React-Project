0068c1697975995ef2d3994df4a807ea51c647e5
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-12-12 21:56:10 +0300

    Basic structure of the Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { Route,Router,Switch } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How to colaborate on gitHub
-          
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/"/>
+          <Route path="/cart"/>
+          <Route path="/contacts"/>
+        </Switch>
+      </Router>
     </div>
   );
 }
