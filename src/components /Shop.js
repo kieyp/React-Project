bf431b02d54ce7556a5c9fe2631b76cf1d7b979a
Boniface <Cheruiyotbony@gmail.com>
2023-12-13 23:58:14 +0300

    shop component update

--- src/components /Shop.js
+++ src/components /Shop.js
@@ -1,11 +1,38 @@
-import React from 'react';
 
-function Shop(){
-    return (
-        <div>
-            <p>Display Goods</p>
+import React, { useEffect, useState } from 'react';
+
+function Shop() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/Stock")
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+        setError("An error occurred while fetching data.");
+      });
+  }, []);
+
+  return (
+    <div className="shop-container"> {/* Add the className here */}
+      {error && <p className="error-message">{error}</p>}
+      {data.map(item => (
+        <div key={item.id} className="shop-item"> {/* Add the className here */}
+
+        <img src={item.Image} alt={`Image for ${item.Name}`} />
+          <p>Name: {item.Name}</p>
+          <p>Description: {item.Description}</p>
+
+          <p>Stock: {item.Stock}</p>
+          {/* Add more details as needed */}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Shop;
